d6e7370c0c477594c75131486105919dc4c6776d
Tung Dang <tungdq2@vng.com.vn>
2020-08-21 17:52:16 +0700

    fix

--- datafile/floats.go
+++ datafile/floats.go
@@ -9,11 +9,11 @@ import (
 func GetFloats(fileName string) ([3]float64, error) {
     var numbers [3]float64
     file, err := os.Open(fileName)
-    if err != nill {
+    if err != nil {
         return numbers, err
     }
     i := 0
-    scanner := os.NewScanner(file)
+    scanner := bufio.NewScanner(file)
     for scanner.Scan() {
         numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
         if err != nil {
